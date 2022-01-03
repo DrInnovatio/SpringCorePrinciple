1c87adb1ad3b12d2896d64bdfcf1778da501e9d3
DrInnovatio <turing1723@gmail.com>
2022-01-03 16:33:54 +0900

    added vip_ok() test.

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -1,7 +1,25 @@
 package hello.core.discount;
 
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class RateDiscountPolicyTest {
 
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP can get 10% discount.")
+    void vip_ok(){
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        int discount = discountPolicy.discount(member, 10000);
+        assertThat(discount).isEqualTo(1000);
+
+    }
 }
