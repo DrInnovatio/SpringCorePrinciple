04222eb75106537addb9473e81157944e615b911
DrInnovatio <turing1723@gmail.com>
2022-01-05 15:28:18 +0900

    test error.

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -2,9 +2,9 @@ package hello.core.beandefinition;
 
 import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.testng.annotations.Test;
 
 public class BeanDefinitionTest {
 
